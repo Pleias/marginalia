aac28996ca379f6a2f50d310c0ced0c365d8c728
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-12 12:22:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='BERTransfer',
+    name='Vllm-json',
     version='0.1.0',
-    description='A Python package for topic transfer based on BERTopic',
+    description='A Python package for structured annotation at scale with LLM',
     author='Pierre-Carl Langlais',
-    url='https://github.com/opinionscience/BERTransfer',
+    url='https://github.com/Pleias/vllm-json',
     packages=find_packages(),
     install_requires=[
-        'numpy', 'pandas', 'bertopic', 'sentence_transformers'
+        'vllm'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
     ],
 )
