d9815216bf6e3a7e49c4bf1ff374caeb115f4832
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-12 13:01:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Vllm-json',
+    name='marginalia',
     version='0.1.0',
     description='A Python package for structured annotation at scale with LLM',
     author='Pierre-Carl Langlais',
-    url='https://github.com/Pleias/vllm-json',
+    url='https://github.com/Pleias/marginalia',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
