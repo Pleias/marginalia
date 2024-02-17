b458c5d1b39fb90bcdd6d7c5d55eaf98286003e8
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-17 21:57:42 +0100

    Update and rename data_entity.py to marginalia_function.py

--- marginalia/data_entity.py
+++
@@ -1,7 +0,0 @@
-class data_entity:
-    def __init__(self, field = None, definition = None, choice=None, min_length = None, literal_check = False):
-        self.field = field
-        self.definition = definition
-        self.choice = choice
-        self.min_length = min_length
-        self.literal_check = literal_check
