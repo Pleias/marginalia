9dc7e80096b7508e211bf82ab90e64506416b056
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-18 00:44:10 +0100

    Update data_entity.py

--- marginalia/data_entity.py
+++ marginalia/data_entity.py
@@ -1,8 +1,9 @@
 #The class for each entry in the data scheme.
 class data_entity:
-    def __init__(self, field = None, definition = None, choice=None, min_length = None, literal_check = False):
+    def __init__(self, field = None, type_field = None, definition = None, choice=None, min_length = None, literal_check = False):
         self.field = field
         self.definition = definition
+        self.type_field = type_field
         self.choice = choice
         self.min_length = min_length
         self.literal_check = literal_check
