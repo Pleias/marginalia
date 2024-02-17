559282c47bd2da857d5aa49db815adf105997599
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-17 21:59:50 +0100

    Create data_entity.py

---
+++ marginalia/data_entity.py
@@ -0,0 +1,8 @@
+#The class for each entry in the data scheme.
+class data_entity:
+    def __init__(self, field = None, definition = None, choice=None, min_length = None, literal_check = False):
+        self.field = field
+        self.definition = definition
+        self.choice = choice
+        self.min_length = min_length
+        self.literal_check = literal_check
