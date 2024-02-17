cd01c4fe0edbcf7f6040ea0275c650fdf481f353
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-18 00:44:54 +0100

    Update marginalia_function.py

--- marginalia/marginalia_function.py
+++ marginalia/marginalia_function.py
@@ -1,1 +1,10 @@
 
+#A function to apply a threshold of text length.
+#Especially useful to enforce reasoning.
+def evaluate_length(valid_json, data_scheme, entry):
+  for data_element in data_scheme:
+    if data_element.field in entry:
+      if data_element.min_length is not None:
+        if len(entry[data_element.field].split)<=data_element.min_length:
+          valid_json = False
+  return valid_json
