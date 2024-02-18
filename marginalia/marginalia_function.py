4f521baad19945c5cdfb60089f6c51150d2a20ab
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-18 01:33:32 +0100

    Update marginalia_function.py

--- marginalia/marginalia_function.py
+++ marginalia/marginalia_function.py
@@ -13,23 +13,9 @@ def evaluate_data_conformity(valid_json, data_scheme, entry):
 
       #Evaluation 2: choices
       if data_element.choice is not None:
-        print(entry[data_element.field])
         choice_result = re.findall(data_element.choice_regex, entry[data_element.field].lower())
         if len(choice_result) > 0:
           choice_result = choice_result[0]
-          entry[data_element.field] = choice_result
-        else:
-          valid_json = False
-  return entry, valid_json
-
-#A function to apply a limitating list of choices
-def evaluate_choices(valid_json, data_scheme, entry):
-  for data_element in data_scheme:
-    if data_element.field in entry:
-      if data_element.choice is not None:
-        choice_result = re.findall(data_element.choice_regex, entry[data_element.field])
-        if (choice_result and choice_result.groups()[1]) is not None:
-          choice_result = choice_result.group(1)
           entry[data_element.field] = choice_result
         else:
           valid_json = False
