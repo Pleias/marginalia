8007bb36fae1ccdcae9d1504d1e558bb206978b5
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-18 01:09:47 +0100

    Update marginalia_function.py

--- marginalia/marginalia_function.py
+++ marginalia/marginalia_function.py
@@ -4,10 +4,19 @@
 def evaluate_length(valid_json, data_scheme, entry):
   for data_element in data_scheme:
     if data_element.field in entry:
+      
+      #Evaluation 1: length
       if data_element.min_length is not None:
-        print(entry)
-        print(entry[data_element.field])
         if len(entry[data_element.field].split())<=data_element.min_length:
+          valid_json = False
+
+      #Evaluation 2: choices
+      if data_element.choice is not None:
+        choice_result = re.findall(data_element.choice_regex, entry[data_element.field].lower())
+        if (choice_result and choice_result.groups()[1]) is not None:
+          choice_result = choice_result.group(1)
+          entry[data_element.field] = choice_result
+        else:
           valid_json = False
   return valid_json
 
