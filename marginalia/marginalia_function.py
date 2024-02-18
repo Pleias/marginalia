9810f6a3a1aa7ea8bc8a3aa96a3771df084f2495
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-18 01:15:54 +0100

    Update marginalia_function.py

--- marginalia/marginalia_function.py
+++ marginalia/marginalia_function.py
@@ -2,6 +2,7 @@
 #A function to apply a threshold of text length.
 #Especially useful to enforce reasoning.
 def evaluate_data_conformity(valid_json, data_scheme, entry):
+  import re
   for data_element in data_scheme:
     if data_element.field in entry:
       
@@ -12,7 +13,9 @@ def evaluate_data_conformity(valid_json, data_scheme, entry):
 
       #Evaluation 2: choices
       if data_element.choice is not None:
+        print(entry[data_element.field])
         choice_result = re.findall(data_element.choice_regex, entry[data_element.field].lower())
+        print(choice_result)
         if (choice_result and choice_result.groups()[1]) is not None:
           choice_result = choice_result.group(1)
           entry[data_element.field] = choice_result
